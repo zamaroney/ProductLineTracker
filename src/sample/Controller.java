99cd127329dc86194232c08acda6f3e03982ae4b
zamaroney <zamaroney@gmail.com>
2019-09-19 14:36:41 -0400

    Added a database.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+  @FXML
+  private Button productButton;
+
+  @FXML
+  private Button recordButton;
+
+  @FXML
+  void printProduct(ActionEvent event) {
+    System.out.println("Added new product!");
+  }
+
+  @FXML
+  void printRecord(ActionEvent event) {
+    System.out.println("Recorded new product!");
+  }
 }
