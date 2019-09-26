e52f029c2e7e126f70a943f1c88ad7cc07c36712
zamaroney <zamaroney@gmail.com>
2019-09-26 14:41:03 -0400

    added some nice CSS

---
+++ src/ProductLine/Controller.java
@@ -0,0 +1,102 @@
+/**
+ * Created by: Zachary Maroney
+ *
+ * This is the implements all the gui components and imports all tje sql/FXML libraries.
+ */
+
+package ProductLine;
+
+import static javafx.collections.FXCollections.observableArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+/**
+ * Sets up the different action that many of the gui components do.
+ * @author Zachary Maroney
+ */
+public class Controller {
+
+  final String JDBC_DRIVER = "org.h2.Driver";
+  final String DB_URL = "jdbc:h2:./res/PR";
+  final String USER = "";
+  final String PASS = "";
+
+  //  Database credentials
+  Connection conn = null;
+  // initialize connections
+  Statement stmt = null;
+
+
+  public enum ItemType {
+    AU , VI, AM, VM
+  }
+
+  /**
+   * Button that sends data about a product to the product table.
+   */
+  @FXML
+  private Button productButton;
+
+  /**
+   * Button that sends data about a product to the records.
+   */
+  @FXML
+  private Button recordButton;
+
+  @FXML
+  private ComboBox<String> chooseQuality;
+
+  /**
+   * action event that sends an example product to the product table.
+   */
+  @FXML
+  void printProduct(ActionEvent event) {
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      // use driver manager to get a connection, that needs three arguments)
+
+      //STEP 3: Execute a query
+      stmt = conn.createStatement();
+
+      stmt.executeUpdate(
+          "INSERT INTO Product(type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' )");
+
+      // STEP 4: Clean-up environment
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }
+
+
+  }
+
+  /**
+   * action event that sends a message to the console.
+   */
+  @FXML
+  void printRecord(ActionEvent event) {
+    System.out.println("Recorded new product!");
+  }
+
+  /**
+   * creates the drop down options for the combobox in the produce .
+   */
+  @FXML
+  public void initialize() {
+    chooseQuality.setItems(observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
+    chooseQuality.getSelectionModel().selectFirst();
+    chooseQuality.setEditable(true);
+  }
+}
---
+++ src/ProductLine/Main.java
@@ -0,0 +1,41 @@
+/**
+ * Created by: Zachary Maroney
+ * <p>
+ * runs the program and creates the window for the gui components.
+ */
+
+package ProductLine;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/***
+ * @author Zachary Maroney
+ */
+
+public class Main extends Application {
+
+  /**
+   * @author Zachary Maroney
+   */
+
+  @Override
+
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("productline.fxml"));
+    primaryStage.setTitle("Product Line");
+    primaryStage.setScene(new Scene(root, 530, 520));
+    primaryStage.show();
+  }
+
+  /**
+   * @author Zachary Maroney
+   */
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
